7aeb8c145b00715f83f23ef5b9c9ca1d1a94c36a
Pranshu321 <pranshujain0111@gmail.com>
2023-01-28 23:25:09 +0530

    pop up in the past disaster screen

--- src/pages/PastDisasters.js
+++ src/pages/PastDisasters.js
@@ -2,9 +2,12 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "../css/cssfiles/PastDisasters.css";
 import Header from "../partials/Header";
+import Popup from "../utils/PopUp";
 const PastDisasters = () => {
 	const [data, setdata] = useState([{}]);
-	const [visible, setVisible] = useState(false);
+	const [title, setitle] = useState("");
+	const [text, settext] = useState("");
+	const [openModal, setOpenModel] = useState(false);
 	const Fetching = async () => {
 		fetch(
 			"https://ap-south-1.aws.data.mongodb-api.com/app/hackit-tngat/endpoint/disaster_blogs"
@@ -26,6 +29,9 @@ const PastDisasters = () => {
 			<Header />
 			<div style={{ background: "black" }}>
 				<div style={{ padding: "30px" }}></div>
+				{openModal && (
+					<Popup title={title} text={text} setOpenModel={setOpenModel} />
+				)}
 				<div className="container">
 					{data.map((item, idx) => {
 						return (
@@ -35,7 +41,11 @@ const PastDisasters = () => {
 									{item.duration} {item.year}
 								</p>
 								<button
-									onClick={() => setVisible(true)}
+									onClick={() => {
+										setitle(item.title);
+										settext(item.disaster_Info.slice(0,1000));
+										setOpenModel(true);
+									}}
 									className="mt-12 cursor-pointer btn text-black bg-gradient-to-r from-yell to-high font-semibold hover:bg-[#00C8C8] w-full mb-4 sm:w-auto sm:mb-0"
 									href="#0"
 								>
