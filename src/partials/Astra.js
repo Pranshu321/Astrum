fb1853abedb61ed97a91832bd3d4bc32d4f0698f
Pranshu321 <pranshujain0111@gmail.com>
2023-01-29 11:27:50 +0530

    Astra file

---
+++ src/partials/Astra.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Astra = () => {
+	return (
+		<div>
+			<iframe
+				allow="microphone;"
+				width="350"
+				height="430"
+				src="https://console.dialogflow.com/api-client/demo/embedded/dfdeb32e-d37a-40f2-a754-6da5f455d755"
+			></iframe>
+		</div>
+	);
+};
+
+export default Astra;
