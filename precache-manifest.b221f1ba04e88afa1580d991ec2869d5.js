8eb55f463ed637eba40fe2950d1c6fd71aa1f766
MSamiDev <mohammadsami@duck.com>
2023-01-29 12:13:48 +0530

    Updates

---
+++ precache-manifest.b221f1ba04e88afa1580d991ec2869d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbecb530d2fc2470777273bd14c9892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91227b3340c99bf5c9ff",
+    "url": "/static/css/main.c2c6be2c.chunk.css"
+  },
+  {
+    "revision": "755c80942d5faa1a2a9a",
+    "url": "/static/js/2.d645813d.chunk.js"
+  },
+  {
+    "revision": "f64a0bf194aa429461aed6530b7359f4",
+    "url": "/static/js/2.d645813d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91227b3340c99bf5c9ff",
+    "url": "/static/js/main.95016586.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.95016586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9c4a7228f7d64cd5ac",
+    "url": "/static/js/runtime-main.aa80aacc.js"
+  }
+]);
